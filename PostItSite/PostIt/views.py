bd18959d404fe43c74a0f605fb88d8567c5c60e9
Blue Electric <be@leekcake.moe>
2020-12-22 17:25:15 +0900

    Implement login

--- PostItSite/PostIt/views.py
+++ PostItSite/PostIt/views.py
@@ -1,19 +1,41 @@
+from django.contrib import auth
 from django.contrib.auth.models import User
-from django.http import HttpRequest
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+
 
 
 # Create your views here.
 
 def main(request):
-    pass
+    if not request.user.is_authenticated:
+        return redirect("login")
+
+    return HttpResponse("OK")
 
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect("./")
+
+    if request.method == 'POST':
+        id: str = request.POST.get('id', '')
+        password: str = request.POST.get('pass', '')
+        user = auth.authenticate(request, username=id, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("./")
+
+        context = {"message": "Invalid user or password"}
+        return render(request, 'PostIt/login.html', context)
+
     return render(request, 'PostIt/login.html')
 
 
 def signup(request: HttpRequest):
+    if request.user.is_authenticated:
+        return redirect("./")
+
     if request.method == 'POST':
         id: str = request.POST.get('id', '')
         password: str = request.POST.get('pass', '')
