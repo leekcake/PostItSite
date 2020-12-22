3b98a395ddf4720473be10074eabbba21b3632ab
Blue Electric <be@leekcake.moe>
2020-12-22 23:17:28 +0900

    Add 'created at' on PostIt

--- PostItSite/PostIt/models.py
+++ PostItSite/PostIt/models.py
@@ -5,5 +5,6 @@ from django.db import models
 # Create your models here.
 
 class PostIt(models.Model):
+    created_at = models.DateTimeField('Created at', auto_now_add=True)
     author = models.ForeignKey(User, related_name='postit', on_delete=models.CASCADE)
     content = models.TextField('Content', null=False)
