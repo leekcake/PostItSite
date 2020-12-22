dd8eff9240048519005c0f367edd40415d3a6aa7
Blue Electric <be@leekcake.moe>
2020-12-22 13:31:48 +0900

    Add login template from internet

--- PostItSite/PostItSite/settings.py
+++ PostItSite/PostItSite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'PostIt.apps.PostitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
